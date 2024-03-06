cc3eee9d9163ec4c738c71e74f2ddb3bf557655f
Frograment <frograment@gmail.com>
2024-03-05 22:23:57 -0600

    Code cleaning and optimizing methods

--- src/main/java/com/getechnologies/Repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.getechnologies.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.getechnologies.Entities.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle,String>  {
-
-}
--- src/main/java/com/getechnologies/Services/ResidentListService.java
+++ src/main/java/com/getechnologies/Services/ResidentListService.java
@@ -5,12 +5,12 @@ import java.util.List;
 import com.getechnologies.Entities.OfficialList;
 import com.getechnologies.Entities.ResidentList;
 public interface ResidentListService {
-List<ResidentList> findAll();
-	
-ResidentList save(ResidentList vehicle);
-	
-ResidentList update(ResidentList vehicle);
-	
-ResidentList delete(String id);
-Boolean existsById(String id);
+	List<ResidentList> findAll();
+		
+	ResidentList save(ResidentList vehicle);
+		
+	ResidentList update(ResidentList vehicle);
+		
+	ResidentList delete(String id);
+	Boolean existsById(String id);
 }
